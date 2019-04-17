a9fc592326f0fb86a495da9296b672c19f79a369
alunoti <alunoti@CEPGBESL02010.estacio.acad>
2019-04-17 19:37:16 -0300

    Testando

---
+++ para.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $a=0;
+        $b=0;
+        for($i=0; $i<10; $i++){
+            $a+=10;
+            $b+=5;
+        }
+        echo "(No final do Loop a = $a e b = $b)";
+        // put your code here
+        ?>
+    </body>
+</html>
